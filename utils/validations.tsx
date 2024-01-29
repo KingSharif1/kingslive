01779a1e026adff8224a3f46aaa45de387ac82d5
KingSharif1 <147098256+KingSharif1@users.noreply.github.com>
2024-01-28 23:19:26 -0600

    contact page not finish
    im sleeping

---
+++ utils/validations.tsx
@@ -0,0 +1,18 @@
+import { z } from "zod"
+
+export const validationSchema = z.object({
+  firstname: z.string({
+    required_error: "Name is required.",
+  }),
+  lastname: z.string({
+    required_error: "Name is required.",
+  }),
+  email: z
+    .string({
+      required_error: "Email is required.",
+    })
+    .email(),
+  message: z.string({
+    required_error: "Message is required.",
+  }),
+});
